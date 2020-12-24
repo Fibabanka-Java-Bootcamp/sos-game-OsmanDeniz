66d9900d9e3f3def34c900c264e26110ff52473d
OsmanDeniz <osmandeniz.ceng@gmail.com>
2020-12-24 18:10:20 +0300

    Matris sinifinin planlanan diger fonksiyonlari yazildi.

--- src/com/deniz/Matrix/Matrix.java
+++ src/com/deniz/Matrix/Matrix.java
@@ -1,17 +1,21 @@
 package com.deniz.Matrix;
 
-import java.util.Scanner;
+
+import com.deniz.Message.Message;
 
 public class Matrix {
-    private final Scanner scanner = new Scanner(System.in);
+
     private String[][] sosMatrix;
     private int matrixSize;
+    private final Message message = new Message();
 
-    public Matrix() {
-        setMatrixSize(sayHelloAndSetSize());
+
+    public Matrix(int size) {
+        setMatrixSize(size);
         createMatrix();
         initialMatrix();
     }
+
 
     public int getMatrixSize() {
         return matrixSize;
@@ -23,20 +27,6 @@ public class Matrix {
 
     public String getMatrixCell(int row, int col) {
         return sosMatrix[row][col];
-    }
-
-    public int sayHelloAndSetSize() {
-        int size;
-
-        System.out.println("Kucuklugumuzun oyunu sos basliyor.");
-        System.out.println("Hazirsaniz kemerlerinizi baglayin ve ucusa hazir olun");
-
-        do {
-            System.out.printf("Kare boyutu ne olsun ? ( 3-7 ) :");
-            size = scanner.nextInt();
-
-        } while (size < 3 || size > 7);
-        return size;
     }
 
     private void createMatrix() {
@@ -53,18 +43,34 @@ public class Matrix {
 
     public void drawMatrix() {
         int matrixSize = sosMatrix.length;
-        System.out.print("  ");
+        message.print("  ");
         for (int i = 1; i <= matrixSize; i++) {
-            System.out.print(" " + i);
+            message.print(" " + i);
         }
-        System.out.println();
+        message.empty();
         for (int i = 0; i < matrixSize; i++) {
             for (int j = 0; j < matrixSize; j++) {
-                if (j == 0) System.out.print(i + 1 + " ");
-                System.out.print(" " + sosMatrix[i][j]);
+                if (j == 0) message.print(i + 1 + " ");
+                message.print(" " + sosMatrix[i][j]);
             }
-            System.out.println();
+            message.empty();
         }
+    }
+
+    public void fillMatrix(int row, int column, String ch) {
+        if (isValid(matrixSize, row, column)) {
+            if (checkCell(row, column))
+                sosMatrix[row][column] = ch;
+
+        }
+    }
+
+    public boolean checkCell(int row, int col) {
+        return (sosMatrix[row][col]).equals("-");
+    }
+
+    public boolean isValid(int size, int row, int column) {
+        return (row >= 0 && row <= size) && (column >= 0 && column <= size);
     }
 
 }
