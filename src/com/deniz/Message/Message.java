fe3e372e45184ba98fd95c2bf97d8945e3c307dd
OsmanDeniz <osmandeniz.ceng@gmail.com>
2020-12-24 18:03:38 +0300

    Mesaj sinifinin temel fonsiyonlari  yazildi.

---
+++ src/com/deniz/Message/Message.java
@@ -0,0 +1,20 @@
+package com.deniz.Message;
+
+public class Message {
+    public void divider() {
+        System.out.println("-------------------------------------");
+    }
+
+    public void empty() {
+        System.out.println();
+    }
+
+    public void println(String message) {
+        System.out.println(message);
+    }
+
+    public void print(String message) {
+        System.out.print(message);
+    }
+
+}
