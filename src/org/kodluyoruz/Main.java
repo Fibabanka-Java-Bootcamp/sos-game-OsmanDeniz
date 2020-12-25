85ec0b852758e3a5b6a08e3b26d11e6f296ae9a2
OsmanDeniz <osmandeniz.ceng@gmail.com>
2020-12-25 14:14:59 +0300

    Oyunun ana kodlari main'e yazildi.

--- src/org/kodluyoruz/Main.java
+++ src/org/kodluyoruz/Main.java
@@ -1,7 +1,100 @@
 package org.kodluyoruz;
 
+import com.deniz.Game.Game;
+import com.deniz.Matrix.Matrix;
+import com.deniz.Message.Message;
+
+import java.util.Scanner;
+
 public class Main {
+    private static final Message message = new Message();
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        try {
+
+            Game game = new Game();
+
+            Matrix matrix = new Matrix(game.sayHelloAndSetSize());
+            final int gameTotal = matrix.getMatrixSize() * matrix.getMatrixSize();
+            int row = -1, col = -1, temp = 0;
+
+            message.empty();
+            message.divider();
+            message.println("Oyuna baslayacak olan oyuncu = " + game.getGamer());
+            message.println("Sizin karakteriniz = " + game.getUserCharacter());
+            message.println("Rakip karakter = " + game.getComputerCharacter());
+            message.divider();
+            message.empty();
+
+            while (game.getGameCount() < gameTotal) {
+                if (game.getGamer() == "Computer") {
+                    final int computerScore = game.getComputerPoint();
+                    do {
+                        row = game.assignComputerValue(matrix.getMatrixSize());
+                        col = game.assignComputerValue(matrix.getMatrixSize());
+                    } while (!matrix.checkCell(row, col));
+
+                    matrix.fillMatrix(row, col, game.getComputerCharacter());
+                    message.empty();
+                    message.println("Computer");
+                    message.println("Satir : " + (row + 1) + ", " + (col + 1));
+
+                    matrix.drawMatrix();
+                    game.checkSOS(matrix, row, col, game.getComputerCharacter(), "Computer");
+                    message.println("Skorlar: Computer : " + game.getComputerPoint() + ", User : " + game.getUserPoint());
+                    game.changeGameCount();
+                    if (Math.abs(game.getComputerPoint() - computerScore) > 0) {
+                        message.println("SOS oldugun icin yeniden oyun hakki veriliyor");
+                        game.setGamer("Computer");
+                    } else game.setGamer("User");
+
+                } else {
+                    final int userScore = game.getUserPoint();
+                    message.empty();
+                    message.println("User");
+                    temp = 0;
+                    do {
+                        if (temp > 0) {
+                            message.println("Hata, degerler yeniden aliniyor");
+                        }
+                        message.print("Satir : ");
+                        row = game.getUserInput() - 1;
+                        message.print("Sutun : ");
+                        col = game.getUserInput() - 1;
+                        temp++;
+                    } while (!matrix.isValid(matrix.getMatrixSize() - 1, row, col) || !matrix.checkCell(row, col));
+                    matrix.fillMatrix(row, col, game.getUserCharacter());
+                    matrix.drawMatrix();
+                    game.checkSOS(matrix, row, col, game.getUserCharacter(), "User");
+                    message.println("Skorlar: Computer : " + game.getComputerPoint() + ", User : " + game.getUserPoint());
+                    game.changeGameCount();
+                    if (Math.abs(game.getUserPoint() - userScore) > 0) {
+                        message.println("SOS oldugun icin yeniden oyun hakki veriliyor");
+                        game.setGamer("User");
+                    } else game.setGamer("Computer");
+                }
+
+            }
+            newGame();
+        } catch (Exception exception) {
+            message.println("Hata ile karsilasildi");
+            newGame();
+        }
+    }
+
+    static void newGame() {
+        try {
+            int secim;
+            Scanner scan = new Scanner(System.in);
+            message.println("Yeniden oynamak ister misin?\n 1-EVET | Digerleri - Hayir ");
+
+            secim = scan.nextInt();
+            if (secim == 1) {
+                main(null);
+            } else return;
+        } catch (Exception e) {
+            newGame();
+        }
+
     }
 }
